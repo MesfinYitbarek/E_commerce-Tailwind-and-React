897b1df3005c3498723a3f0c451bdd6756167ae4
MesfinYitbarek <mesfinyitbarek55@gmail.com>
2024-04-18 13:51:22 +0300

    commit

--- e-commerce/src/components/Testimonials/Testimonials.jsx
+++ e-commerce/src/components/Testimonials/Testimonials.jsx
@@ -1,1 +1,27 @@
+import React from 'react'
 
+const Testimonials = () => {
+  return (
+    <div className='py-10 mb-10'>
+        <div className='container'>
+            {/* header section */}
+            <div className='text-center mb-10 max-w[600px]
+            mx-auto'>
+                <p data-aos="fade-up" className='text-sm text-primary '>What our customers are saying </p>
+                <h1 data-aos="fade-up" className='text-3xl font-bold'>Testimonials</h1>
+                <p  data-aos="fade-up" className='text-xs text-gray-400'>Lorem ipsum dolor sit amet, consectetur 
+                   adipiscing elit, sed do eiusmod tempor incididunt ut 
+                   labore et dolore magna aliqua</p>
+                
+            </div>
+            <div>
+                
+            </div>
+
+        </div>
+      
+    </div>
+  )
+}
+
+export default Testimonials
